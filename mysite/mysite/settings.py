70d008e41205e27e50a878a9ff242c9ee7a7b90f
lang <lang1437@163.com>
2017-07-25 17:40:34 +0800

    test

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'   #'Asia/Shanghai'     #
 
 USE_I18N = True
 
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,7 +14,9 @@ class Question(models.Model):
 		return self.question_text
 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+		# return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)	#Bug
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 @python_2_unicode_compatible
 class Choice(models.Model):
